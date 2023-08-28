97abe25c6e834fe523c366b3d5e2bfc13622c3e2
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-08-28 21:08:13 +0800

    Create IGCSE - Variable Number Machine.py
    Exercises for "Variable Number Machine" done

---
+++ IGCSE - Variable Number Machine.py
@@ -0,0 +1,11 @@
+#Exercise 1
+AnwarIbrahimBorn = 1947
+MyAge = int(input('What year were you born in? '))
+AgeDifference = MyAge - AnwarIbrahimBorn
+print("Anwar Ibrahim is",AgeDifference,"years older than you")
+
+#Exercise 2
+CelebrityAge = int(input('What year was your celebrity born in? '))
+MyAge = int(input('What year were you born in? '))
+AgeDifference = MyAge - CelebrityAge
+print("Your celebrity is",AgeDifference,"years older than you")
