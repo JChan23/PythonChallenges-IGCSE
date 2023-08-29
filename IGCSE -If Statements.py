c1ff1f210c2c7d255b497d1850c2db040b1530aa
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-08-29 08:47:21 +0800

    Create IGCSE -If Statements.py
    Exercises done. Includes written questions as comments

---
+++ IGCSE -If Statements.py
@@ -0,0 +1,12 @@
+#Exercise 1
+#a. Results in an indentation error
+#b. Nothing happens, code does not recognise "Y"
+#c. Nothing happens, code no longer recognises "y"
+
+#Exercise 2
+test = input("Is it raining? y/n ")
+if test == "y" or test == "yes":
+  print("Oh dear, no football today!")
+
+#Exercise 3
+#Indentation error
