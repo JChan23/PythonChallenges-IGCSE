587c41ca504cf8f5255dc986245008947010dbdf
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-08-29 08:41:44 +0800

    Create IGCSE - Iffy Numbers.py
    Challenge done

---
+++ IGCSE - Iffy Numbers.py
@@ -0,0 +1,9 @@
+MyHeight = 171
+YourHeight = int(input("How tall are you in centimeters? "))
+
+if MyHeight > YourHeight:
+  print("I'm taller than you!")
+elif MyHeight < YourHeight:
+  print("I'm shorter than you!")
+else:
+  print("We must be the same height!")
