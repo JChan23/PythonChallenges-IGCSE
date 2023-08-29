ac5cf119be0e269361966f9386d3a8566699adcb
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-08-29 08:56:28 +0800

    Create IGCSE - Simple For loops.py
    Challenge done

---
+++ IGCSE - Simple For loops.py
@@ -0,0 +1,12 @@
+#Exercise 1
+#For loops ends at count. So when the for loop hits the number 100, it stops
+for i in range (1,101):
+    print(i)
+
+#Exercise 2
+for i in range (2,101,2):
+    print(i)
+
+#Exercise 3
+for i in range (1000,0,-10):
+    print(i)
