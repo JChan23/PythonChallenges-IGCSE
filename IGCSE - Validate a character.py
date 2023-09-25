a46e1ccbbece37e1f888b17ff3fdcd65093c6a11
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-09-25 08:14:41 +0800

    Create IGCSE - Validate a character.py

---
+++ IGCSE - Validate a character.py
@@ -0,0 +1,6 @@
+character = str(input('Please input a single character: '))
+length = len(character)
+if length != 1:
+  print('Error. Input is not a single character. Please try again')
+else:
+  print('Input accepted')
