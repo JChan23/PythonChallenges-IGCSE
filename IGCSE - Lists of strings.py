05180d1ce0a77d23739643caddff37148ff71c7e
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-10-09 08:19:36 +0800

    Create IGCSE - Lists of strings.py

---
+++ IGCSE - Lists of strings.py
@@ -0,0 +1,9 @@
+#Exercise 1
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+print(list[0:4], sep=", ") 
+
+#Exercise 2
+characters = ['Mario', 'Sonic', 'Link']
+NewCharacter = input('Please input a new character into the list: ')
+characters.append(NewCharacter)
+print(characters) 
