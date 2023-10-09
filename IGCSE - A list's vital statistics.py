dd2d3510d273c3cc5cfaedd9f1a5fc7fe82ae0ee
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-10-09 08:25:07 +0800

    Create IGCSE - A list's vital statistics.py

---
+++ IGCSE - A list's vital statistics.py
@@ -0,0 +1,12 @@
+#Exercise 1
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+print(sum(numbers))
+print('')
+
+#Exercise 2
+print('Numerical statistics of list:')
+print('Total:', sum(numbers))
+print('Minimum value:', min(numbers))
+print('Maxumum value:', max(numbers))
+print('Length of list:', len(numbers))
+print('Average value of elements:', sum(numbers)/len(numbers))
