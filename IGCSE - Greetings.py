d5f44c98c74a320a8e79fc4107533fa64e95d8ae
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-10-31 19:29:21 +0800

    Create IGCSE - Greetings.py

---
+++ IGCSE - Greetings.py
@@ -0,0 +1,5 @@
+#Create a program to ask for somebody's name and age and then greet them.	
+name = input('What is your name? ')
+print('Hello', name, '\n')
+age = input('How old are you? ')
+print('Nice!')
